de035f9f1d85c844a5b35321f6cc0047a2d039c2
Simon Kunz <pattexman@web.de>
2015-04-28 21:28:46 +0200

    added appache, changed font, polishing

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,16 @@ module.exports = function (grunt) {
     });
 
     grunt.initConfig({
+        appcache: {
+            options: {
+                basePath: '*/assets'
+            },
+            all: {
+                dest: 'manifest.appcache',
+                cache: 'assets/**/*',
+                network: '*'
+            }
+        },
         scsslint: {
             all: [
                 '_dev/**/*.scss'
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" manifest="manifest.appcache">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Diablo 3 Profile App made with reactjs"/>
     <title>D3 Profile App</title>
     <link rel="stylesheet" href="/assets/css/styles.css" />
+    <link href='http://fonts.googleapis.com/css?family=Oxygen:700' rel='stylesheet' type='text/css'>
 </head>
 <body>
