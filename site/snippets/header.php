f8242b21d5cf530161d6d5c192493ce162d8b720
Simon Kunz <pattexman@web.de>
2015-04-28 23:46:28 +0200

    title change

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Diablo 3 Profile App made with reactjs"/>
     <link rel="shortcut icon" type="image/png" href="<?php echo url("favicon.ico"); ?>">
-    <title>D3 Profile App</title>
+    <title>D3 Profile</title>
     <link rel="stylesheet" href="/assets/css/styles.min.css" />
     <link href='http://fonts.googleapis.com/css?family=Oxygen:700' rel='stylesheet' type='text/css'>
 </head>
