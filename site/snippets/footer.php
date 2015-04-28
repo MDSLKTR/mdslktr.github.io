70fc1a5a43a27ec47fe85444351451dc9a69d378
Simon Kunz <pattexman@web.de>
2015-04-28 22:17:32 +0200

    updated a few things

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,5 @@
     <footer role="contentinfo">
-        <span>&copy Simon Kunz - 2015</span>
+        <span>&copy Simon Kunz - 2015 | Icons & Game Texts &copy Blizzard</span>
     </footer>
     <script src="/assets/js/vendor.min.js"></script>
     <script src="/assets/js/scripts.min.js"></script>
